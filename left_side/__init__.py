1f1daf1d4194622250e9192f70555797324d2d14
liho-o <67860443+liho-o@users.noreply.github.com>
2024-02-12 21:56:51 +0700

    structure

---
+++ left_side/__init__.py
@@ -0,0 +1,1 @@
+from left_side import turtule_part
---
+++ main.py
@@ -0,0 +1,12 @@
+from left_side import turtule_part
+import turtle
+
+
+
+def main():
+    turtule_part.left_part()
+
+
+if __name__ == '__main__':
+    main()
+    turtle.done()
