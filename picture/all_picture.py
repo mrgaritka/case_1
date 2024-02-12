5074c426b01818819aadf16224aeaeac221763a8
liho-o <67860443+liho-o@users.noreply.github.com>
2024-02-13 00:14:22 +0700

    add rotation and triangle def

--- picture/all_picture.py
+++ picture/all_picture.py
@@ -1,8 +1,10 @@
-from regular_figuer.all_figure import square
+from regular_figuer.all_figure import square, triangle
 
 
 
 #Делаем функция под каждую картинку
 def fish(x, y):
-    square(-200, 200, "red")  #Состовляем из готовых функций картинки
-    square(-100, 300, "red", scale=1.2)  #вариант с увеличаением фигуры на 20%
+    # square(-200, 200, "red")  #Состовляем из готовых функций картинки
+    square(-100, 300, "blue", 30, scale=1.2)  #вариант с увеличаением фигуры на 20%
+    triangle(200, 200, "blue", 30, scale=1.2)  #вариант с увеличаением фигуры на 20%
+
--- regular_figuer/all_figure.py
+++ regular_figuer/all_figure.py
@@ -1,13 +1,40 @@
 import turtle
 
-def square(x, y, fill_color, scale = 1):
+def square(x, y, fill_color, rotation=0, scale = 1):
     a = 100 * scale
     turtle.up()  # Поднять перо (не оставлять след при перемещении)
     turtle.setposition(x, y)  # Установка позиции
     turtle.down()  # Опустить перо (оставлять след при перемещении)
     turtle.fillcolor(fill_color)  # Установка цвета заливки
     turtle.begin_fill()  # Начать заливку фигуры
-    for i in range(4):  # Цикл для рисования квадрата
-        turtle.forward(a)  # Переместиться вперед на длину стороны
-        turtle.right(90)  # Повернуть направо на 90 градусов
+
+    turtle.right(rotation)  # Повернуть
+    turtle.forward(a)  # Переместиться вперед на длину стороны
+
+    turtle.right(90)  # Повернуть направо на 90 градусов
+    turtle.forward(a)  # Переместиться вперед на длину стороны
+
+    turtle.right(90)  # Повернуть направо на 90 градусов
+    turtle.forward(a)  # Переместиться вперед на длину стороны
+
+
     turtle.end_fill()  # Завершить заливку фигуры
+
+
+def triangle(x, y, fill_color, rotation=0, scale=1):
+    a = 100 * scale
+
+    turtle.up()  # Поднять перо (не оставлять след при перемещении)
+    turtle.setposition(x, y)  # Установка позиции
+    turtle.down()  # Опустить перо (оставлять след при перемещении)
+    turtle.fillcolor(fill_color)  # Установка цвета заливки
+    turtle.begin_fill()  # Начать заливку фигуры
+
+    turtle.right(rotation)
+    turtle.forward(a)  # Переместиться вперед на длину стороны
+
+    turtle.right(90)  # Повернуть направо на 90 градусов
+    turtle.forward(a)  # Переместиться вперед на длину стороны
+    turtle.end_fill()  # Завершить заливку фигуры
+
+
