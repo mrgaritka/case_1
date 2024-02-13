aa6911e377d57bdda697d74047b12686b9c0a60d
mrgaritka <m.kabanova@g.nsu.ru>
2024-02-13 13:04:49 +0300

    parallelogram

--- regular_figuer/all_figure.py
+++ regular_figuer/all_figure.py
@@ -17,6 +17,7 @@ def square(x, y, fill_color, rotation=0, scale = 1):
     turtle.right(90)  # Повернуть направо на 90 градусов
     turtle.forward(a)  # Переместиться вперед на длину стороны
 
+    turtle.right(180)
 
     turtle.end_fill()  # Завершить заливку фигуры
 
@@ -34,7 +35,32 @@ def triangle(x, y, fill_color, rotation=0, scale=1):
     turtle.forward(a)  # Переместиться вперед на длину стороны
 
     turtle.right(90)  # Повернуть направо на 90 градусов
-    turtle.forward(a)  # Переместиться вперед на длину стороны
+    turtle.forward(a)# Переместиться вперед на длину стороны
+
+    turtle.right(270)
     turtle.end_fill()  # Завершить заливку фигуры
 
+def parallelogram(x, y, fill_color, rotation=0, scale = 1):
+    a = 100 * scale
+    b = 50 * scale
+    turtle.up()  # Поднять перо (не оставлять след при перемещении)
+    turtle.setposition(x, y)  # Установка позиции
+    turtle.down()  # Опустить перо (оставлять след при перемещении)
+    turtle.fillcolor(fill_color)  # Установка цвета заливки
+    turtle.begin_fill()  # Начать заливку фигуры
+
+    turtle.right(rotation)  # Повернуть
+    turtle.forward(a)  # Переместиться вперед на длину стороны
+
+    turtle.right(120)  # Повернуть направо на 90 градусов
+    turtle.forward(b)  # Переместиться вперед на длину стороны
+
+    turtle.right(60)  # Повернуть направо на 90 градусов
+    turtle.forward(a) # Переместиться вперед на длину стороны
+
+    turtle.right(120)  # Повернуть направо на 90 градусов
+    turtle.forward(b)
+
+    turtle.right(60)
+    turtle.end_fill()  # Завершить заливку фигуры
 
