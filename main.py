baf571821e63ced832276fcc8ab73d197dbc4c39
alice-kim-05 <alice_kim_05@mail.ru>
2024-02-13 21:56:45 +0700

    add changes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from picture.all_picture import fish, helicopter, rabbit, rocket, ship
+from picture.all_picture import fish, helicopter, rabbit, rocket, ship, humen, rooster
 import turtle
 
 
@@ -9,6 +9,8 @@ def main():
     helicopter(0,0)
     rabbit(0,0)
     rocket(0,0)
+    humen(0,0)
+    rooster(0,0)
     ship(0,0)
     turtle.done() # Завершаем рисовать
 
