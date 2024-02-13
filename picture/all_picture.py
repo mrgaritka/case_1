6372129baaf39983f17b245ae9befcc741149716
alice-kim-05 <alice_kim_05@mail.ru>
2024-02-13 19:04:15 +0700

    add fish

--- picture/all_picture.py
+++ picture/all_picture.py
@@ -5,10 +5,13 @@ from regular_figuer.all_figure import square, triangle, parallelogram
 
 #Делаем функция под каждую картинку
 def fish(x, y):
-    # square(-200, 200, "red")  #Состовляем из готовых функций картинки
-    square(-100, 300, "blue", 0, scale=1.2)  #вариант с увеличаением фигуры на 20%
-    triangle(200, 200, "blue", 0, scale=1.2)  #вариант с увеличаением фигуры на 20%
-    parallelogram(-300, 300, "blue", 0, scale=1.2)
+    square(-100, 300, "orange", 45, scale=1)  # вариант с увеличаением фигуры на 20%
+    triangle(-27, 300, "blue", 0, scale=1)  # вариант с увеличаением фигуры на 20%
+    triangle(-177, 375, "red", 315, scale=1.5)  # вариант с увеличаением фигуры на 20%
+    triangle(-28, 230, "yellow", 90, scale=1.5)  # вариант с увеличаением фигуры на 20%
+    triangle(-172, 230, "purple", 90, scale=0.5)  # вариант с увеличаением фигуры на 20%
+    triangle(-272, 280, "pink", 180, scale=0.5)  # вариант с увеличаением фигуры на 20%
+    parallelogram(-172, 230, "green", 90, scale=1)  # вариант с увеличаением фигуры на 20%
 
 def helicopter(x,y):
     parallelogram(0, 300, "green", 0, scale=1)
