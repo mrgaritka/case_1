d4c39ee72165cbd9fbb87951f8f3a41d3acc9f60
alice-kim-05 <alice_kim_05@mail.ru>
2024-02-13 18:38:12 +0700

    add file

--- picture/all_picture.py
+++ picture/all_picture.py
@@ -1,10 +1,13 @@
-from regular_figuer.all_figure import square, triangle
-
+from regular_figuer.all_figure import square, triangle, parallelogram
 
 
 #Делаем функция под каждую картинку
 def fish(x, y):
     # square(-200, 200, "red")  #Состовляем из готовых функций картинки
-    square(-100, 300, "blue", 30, scale=1.2)  #вариант с увеличаением фигуры на 20%
-    triangle(200, 200, "blue", 30, scale=1.2)  #вариант с увеличаением фигуры на 20%
-
+    square(-100, 300, "orange", 45, scale=1)  #вариант с увеличаением фигуры на 20%
+    triangle(-27, 300, "blue", 0, scale=1)  #вариант с увеличаением фигуры на 20%
+    triangle(-177, 375, "red", 315, scale=1.5)  # вариант с увеличаением фигуры на 20%
+    triangle(-28, 230, "yellow", 90, scale=1.5)  # вариант с увеличаением фигуры на 20%
+    triangle(-172, 230, "purple", 90, scale=0.5)  # вариант с увеличаением фигуры на 20%
+    triangle(-272, 280, "pink", 180, scale=0.5)  # вариант с увеличаением фигуры на 20%
+    parallelogram(-172, 230, "green", 90, scale=1)  # вариант с увеличаением фигуры на 20%
